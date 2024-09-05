e145b92caf018870638ad60055a3e4c9ad3e1e7a
John <johneliud4@gmail.com>
2024-09-05 04:01:25 +0300

    Add: Function NextToken() returns the next token in the input. Uses conditional statements in a switch case to determine what character to return based on the encountered token.

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -24,3 +24,74 @@ func (l *Lexer) readChar() {
 	l.position = l.readPosition
 	l.readPosition++
 }
+
+// NextToken will return the next token in the input
+func (l *Lexer) NextToken() token.Token {
+	var tok token.Token
+
+	l.skipWhiteSpace()
+
+	switch l.ch {
+	case '=':
+		if l.peekChar() == '=' {
+			ch := l.ch
+			l.readChar()
+			literal := string(ch) + string(l.ch)
+			tok = token.Token{Type: token.EQUAL, Literal: literal}
+		} else {
+			tok = newToken(token.ASSIGN, l.ch)
+		}
+
+	case '+':
+		tok = newToken(token.PLUS, l.ch)
+	case '-':
+		tok = newToken(token.MINUS, l.ch)
+	case '!':
+		if l.peekChar() == '=' {
+			ch := l.ch
+			l.readChar()
+			literal := string(ch) + string(l.ch)
+			tok = token.Token{Type: token.NOT_EQUAL, Literal: literal}
+		} else {
+			tok = newToken(token.NOT, l.ch)
+		}
+	case '*':
+		tok = newToken(token.ASTERISK, l.ch)
+	case '/':
+		tok = newToken(token.SLASH, l.ch)
+	case '<':
+		tok = newToken(token.LESS_THAN, l.ch)
+	case '>':
+		tok = newToken(token.GREATER_THAN, l.ch)
+	case ',':
+		tok = newToken(token.COMMA, l.ch)
+	case ';':
+		tok = newToken(token.SEMI_COLON, l.ch)
+	case '(':
+		tok = newToken(token.LEFT_PARENTHESIS, l.ch)
+	case ')':
+		tok = newToken(token.RIGHT_PARENTHESIS, l.ch)
+	case '{':
+		tok = newToken(token.LEFT_BRACE, l.ch)
+	case '}':
+		tok = newToken(token.RIGHT_BRACE, l.ch)
+	case 0:
+		tok.Literal = ""
+		tok.Type = token.EOF
+	default:
+		if isLetter(l.ch) {
+			tok.Literal = l.readIdentifier()
+			tok.Type = token.LookupIdent(tok.Literal)
+			return tok
+		} else if isDigit(l.ch) {
+			tok.Type = token.INT
+			tok.Literal = l.readNumber()
+			return tok
+		} else {
+			tok = newToken(token.ILLEGAL, l.ch)
+		}
+	}
+
+	l.readChar()
+	return tok
+}
