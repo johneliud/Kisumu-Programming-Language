20a9c295208c82da30319dc7f189e1bca111f4dd
John <johneliud4@gmail.com>
2024-09-11 16:50:21 +0300

    Chore: Implement language parser logic that is able to generate tokens from a source code and compare against the keywords supported by the language's tokens.

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -2,6 +2,7 @@ package lexer
 
 import "github.com/johneliud/Kisumu-Programming-Language/token"
 
+// Lexer holds the input string and manages the current reading position.
 type Lexer struct {
 	input        string
 	position     int
@@ -9,12 +10,15 @@ type Lexer struct {
 	ch           byte
 }
 
+
+// Initializes a new Lexer instance with the input string and reads the first character.
 func New(input string) *Lexer {
 	l := &Lexer{input: input}
 	l.readChar()
 	return l
 }
 
+// Reads the next character from the input and advances positions.
 func (l *Lexer) readChar() {
 	if l.readPosition >= len(l.input) {
 		l.ch = 0
@@ -25,11 +29,12 @@ func (l *Lexer) readChar() {
 	l.readPosition++
 }
 
+// Analyzes the current character and returns the next token.
 func (l *Lexer) NextToken() token.Token {
 	var tok token.Token
-
 	l.skipWhiteSpace()
 
+	// Analyze the current character to determine the token type
 	switch l.ch {
 	case '=':
 		if l.peekChar() == '=' {
@@ -104,6 +109,7 @@ func (l *Lexer) NextToken() token.Token {
 	return tok
 }
 
+// Reads a string literal until the closing quote or end of input.
 func (l *Lexer) readString() string {
 	position := l.position + 1
 	for {
@@ -116,10 +122,12 @@ func (l *Lexer) readString() string {
 	return l.input[position:l.position]
 }
 
+// Creates a new token with the specified type and literal value.
 func newToken(tokenType token.TokenType, ch byte) token.Token {
 	return token.Token{Type: tokenType, Literal: string(ch)}
 }
 
+// Reads a sequence of letters from the input.
 func (l *Lexer) readIdentifier() string {
 	position := l.position
 	for isLetter(l.ch) {
@@ -128,10 +136,12 @@ func (l *Lexer) readIdentifier() string {
 	return l.input[position:l.position]
 }
 
+// Checks if a character is a valid letter or underscore.
 func isLetter(ch byte) bool {
 	return 'a' <= ch && ch <= 'z' || 'A' <= ch && ch <= 'Z' || ch == '_'
 }
 
+// Reads a sequence of digits from the input.
 func (l *Lexer) readNumber() string {
 	position := l.position
 	for isDigit(l.ch) {
@@ -140,16 +150,19 @@ func (l *Lexer) readNumber() string {
 	return l.input[position:l.position]
 }
 
+// Checks if a character is a valid digit.
 func isDigit(ch byte) bool {
 	return '0' <= ch && ch <= '9'
 }
 
+// Skips over spaces, tabs, and newlines.
 func (l *Lexer) skipWhiteSpace() {
 	for l.ch == ' ' || l.ch == '\t' || l.ch == '\n' || l.ch == '\r' {
 		l.readChar()
 	}
 }
 
+// Returns the next character in the input without advancing the position.
 func (l *Lexer) peekChar() byte {
 	if l.readPosition >= len(l.input) {
 		return 0
